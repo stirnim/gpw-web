7171261cda82f2d1ed09cb0bf4fef42569d6b1b4
Daniel Stirnimann <daniel.stirnimann@switch.ch>
2024-09-29 10:24:07 +0200

    fix nav

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     var navbar = document.querySelector('.navbar');
-    var submenuItems = document.querySelectorAll('.has-submenu');
+    var navItems = document.querySelectorAll('.nav-item.has-submenu');
+    var submenuTimeout;
 
+    // Function to handle scroll event
     window.addEventListener('scroll', function() {
         if (window.scrollY > 0) {
             navbar.classList.add('scrolled');
@@ -10,13 +12,28 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    // Add event listeners to show/hide the extended gradient when submenu is open
-    submenuItems.forEach(function(item) {
-        item.addEventListener('mouseenter', function() {
-            navbar.classList.add('submenu-open');
-        });
-        item.addEventListener('mouseleave', function() {
+    // Function to show the submenu
+    function showSubmenu() {
+        clearTimeout(submenuTimeout);
+        navbar.classList.add('submenu-open');
+    }
+
+    // Function to hide the submenu after a delay
+    function hideSubmenu() {
+        submenuTimeout = setTimeout(function() {
             navbar.classList.remove('submenu-open');
-        });
+        }, 400); // Adjust the delay (in milliseconds) as needed
+    }
+
+    // Add event listeners to nav-items and their submenus
+    navItems.forEach(function(item) {
+        item.addEventListener('mouseenter', showSubmenu);
+        item.addEventListener('mouseleave', hideSubmenu);
+
+        var submenu = item.querySelector('.submenu');
+        if (submenu) {
+            submenu.addEventListener('mouseenter', showSubmenu);
+            submenu.addEventListener('mouseleave', hideSubmenu);
+        }
     });
 });
