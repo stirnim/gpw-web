afc6533fec8364b1db7451601b5d64bc9c3fe0f1
Daniel Stirnimann <daniel.stirnimann@switch.ch>
2024-10-01 20:04:40 +0200

    fix nav: open all submenus at once

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var navbar = document.querySelector('.navbar');
-    var navItems = document.querySelectorAll('.nav-item.has-submenu');
     var submenuTimeout;
-    var submenuOpen = false;
 
     // Function to handle scroll event
     window.addEventListener('scroll', function() {
@@ -13,59 +11,25 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    // Function to show the submenu
-    function showSubmenu() {
+    // Function to show all submenus
+    function showAllSubmenus() {
         clearTimeout(submenuTimeout);
-        if (!submenuOpen) {
-            navbar.classList.add('submenu-open');
-            submenuOpen = true;
-        }
+        navbar.classList.add('submenu-open');
     }
 
-    // Function to hide the submenu after a delay
-    function hideSubmenu() {
+    // Function to hide all submenus after a delay
+    function hideAllSubmenus() {
         submenuTimeout = setTimeout(function() {
             navbar.classList.remove('submenu-open');
-            submenuOpen = false;
-        }, 400); // Adjust the delay as needed
+        }, 200); // Adjust the delay as needed
     }
 
-    // Function to check if the mouse is moving within the submenu area
-    function isMouseInside(event, parent) {
-        var relatedTarget = event.relatedTarget;
-        while (relatedTarget) {
-            if (relatedTarget === parent) {
-                return true;
-            }
-            relatedTarget = relatedTarget.parentNode;
-        }
-        return false;
-    }
+    // Add event listeners to the navbar
+    navbar.addEventListener('mouseenter', function() {
+        showAllSubmenus();
+    });
 
-    // Add event listeners to nav-items and their submenus
-    navItems.forEach(function(item) {
-        var submenu = item.querySelector('.submenu');
-
-        item.addEventListener('mouseover', function(event) {
-            showSubmenu();
-        });
-
-        item.addEventListener('mouseout', function(event) {
-            if (!isMouseInside(event, item)) {
-                hideSubmenu();
-            }
-        });
-
-        if (submenu) {
-            submenu.addEventListener('mouseover', function(event) {
-                showSubmenu();
-            });
-
-            submenu.addEventListener('mouseout', function(event) {
-                if (!isMouseInside(event, item)) {
-                    hideSubmenu();
-                }
-            });
-        }
+    navbar.addEventListener('mouseleave', function() {
+        hideAllSubmenus();
     });
 });
