8b9ecf6f961a4ad5cfba83927c3a0ca4a1a90bb1
Daniel Stirnimann <daniel.stirnimann@switch.ch>
2024-10-04 11:11:49 +0200

    fix nav: initial mobile navigation

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,6 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     var navbar = document.querySelector('.navbar');
+    var mobileMenuIcon = document.querySelector('.mobile-menu-icon');
     var submenuTimeout;
+
+    // Toggle Mobile Navigation Menu
+    mobileMenuIcon.addEventListener('click', function() {
+        var isActive = navbar.classList.toggle('mobile-nav-active');
+        document.body.classList.toggle('mobile-nav-active');
+        mobileMenuIcon.classList.toggle('is-active');
+        mobileMenuIcon.setAttribute('aria-expanded', isActive);
+    });
+
+    // Toggle Submenus on Mobile
+    var navItems = document.querySelectorAll('.nav-item.has-submenu');
+    navItems.forEach(function(item) {
+        var link = item.querySelector('.nav-link');
+        link.addEventListener('click', function(event) {
+            if (window.innerWidth <= 810) {
+                event.preventDefault();
+                item.classList.toggle('mobile-submenu-active');
+            }
+        });
+    });
 
     // Function to handle scroll event
     function handleScroll() {
@@ -17,21 +38,21 @@ document.addEventListener("DOMContentLoaded", function() {
     // Add event listener for scroll event
     window.addEventListener('scroll', handleScroll);
 
-    // Function to show submenus (from previous code)
+    // Function to show submenus
     function showSubmenus() {
         clearTimeout(submenuTimeout);
         navbar.classList.add('submenu-open');
     }
 
-    // Function to hide submenus (from previous code)
+    // Function to hide submenus
     function hideSubmenus() {
         clearTimeout(submenuTimeout);
         submenuTimeout = setTimeout(function() {
             navbar.classList.remove('submenu-open');
-        }, 200); // Adjust the delay as needed
+        }, 200);
     }
 
-    // Event listeners for submenu handling (from previous code)
+    // Event listeners for submenu handling
     var navLinksContainer = document.querySelector('.nav-links-container');
     navLinksContainer.addEventListener('mouseenter', showSubmenus);
     navLinksContainer.addEventListener('mouseleave', hideSubmenus);
